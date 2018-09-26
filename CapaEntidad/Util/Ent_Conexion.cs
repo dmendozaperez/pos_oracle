685166f466040f99c335aa45ef1c1d2aa8f1fb49
DMendoza <DMendoza@PORTSIST18>
2018-09-26 12:26:26 -0500

    update conexion

--- CapaEntidad/Util/Ent_Conexion.cs
+++ CapaEntidad/Util/Ent_Conexion.cs
@@ -34,10 +34,10 @@ namespace CapaEntidad.Util
         #endregion
 
         #region<CONEXION PRODUCCION>
-        public static string ftp_server { get { return "172.16.24.216"; } }
-        public static string ftp_user { get { return "webposbpe"; } }
-        public static string ftp_password { get { return "JU737CbDmJvu"; } }
-        public static Int32 ftp_puerto { get { return 22; } }
+        //public static string ftp_server { get { return "172.16.24.216"; } }
+        //public static string ftp_user { get { return "webposbpe"; } }
+        //public static string ftp_password { get { return "JU737CbDmJvu"; } }
+        //public static Int32 ftp_puerto { get { return 22; } }
 
         #endregion
         #endregion

99f48412dcece4cad5659c53db19b2a25745bbda
DMendoza <DMendoza@PORTSIST18>
2019-10-30 12:37:47 -0500

    Agregando capas para transaction de 64x

---
+++ ServiceXstore/Install_Xstore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceXstore
+{
+    [RunInstaller(true)]
+    public partial class Install_Xstore : System.Configuration.Install.Installer
+    {
+        public Install_Xstore()
+        {
+            InitializeComponent();
+        }
+    }
+}

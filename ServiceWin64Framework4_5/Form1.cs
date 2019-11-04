4a754cb2df223daa5eeecd63228dee8f29c59fa5
Enrique Heredia <enriqueheredia.e@outlook.com>
2019-11-04 08:39:24 -0500

    prueba servicio

--- ServiceWin64Framework4_5/Form1.cs
+++ ServiceWin64Framework4_5/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CapaServicioWindows_x64.Envio_Ftp_Xstore;
+using CapaServicioWindows_x64.Modular;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,26 @@ namespace ServiceWin64Framework4_5
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string pais = "PE";
+            Boolean gen_per_item = false;
+            Boolean gen_ecu_item = false;
+            string _error = "";
+            //Ftp_Xstore_Service_Send ejecuta_procesos = null;        
+            //ejecuta_procesos = new Ftp_Xstore_Service_Send();
+            //ejecuta_procesos.ejecutar_genera_file_xstore_auto(pais, ref _error, ref gen_per_item, ref gen_ecu_item);
+
+            Xstore_Genera_Inter ejecuta_procesos_inter = new Xstore_Genera_Inter();
+            ejecuta_procesos_inter.ejecutar_genera_interface_xstore(ref _error);
+            MessageBox.Show(_error);
         }
     }
 }
